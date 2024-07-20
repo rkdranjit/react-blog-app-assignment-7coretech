9b8c45205d1388af80d033ae027d0c802b1d5b86
Ranjit Kumar Das <48598102+rkdranjit@users.noreply.github.com>
2024-07-20 23:18:40 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://newsapi.org/v2/everything";
-const API_KEY = "7a9294b43ce64bf3b1a16d5b5a185676";
+const API_KEY = "YOUR_API_KEY";
 
 export const fetchPosts = async (query = "technology", page = 1) => {
   try {
